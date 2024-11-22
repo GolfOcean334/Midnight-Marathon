730342054852db62c0686bccd4c1609b846d8882
Axel Gathelier <130740013+GolfOcean334@users.noreply.github.com>
2024-11-22 11:20:50 +0100

    Ajout liaison entre les scenes

---
+++ Game-Jam/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void GoMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
